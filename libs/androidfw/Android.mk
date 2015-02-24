VanirAOSP/frameworks_base

frameworks/base: Fix various aliasing violations
Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

core/jni/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_CFLAGS += -DHAVE_CONFIG_H -DKHTML_NO_EXCEPTIONS -DGKWQ_NO_JAVA
 LOCAL_CFLAGS += -DNO_SUPPORT_JS_BINDING -DQT_NO_WHEELEVENT -DKHTML_NO_XBL
 LOCAL_CFLAGS += -U__APPLE__
+LOCAL_CFLAGS += -fno-strict-aliasing
 
 ifeq ($(TARGET_ARCH), arm)
 	LOCAL_CFLAGS += -DPACKED="__attribute__ ((packed))"

chickenfactory/android_frameworks_base-1

Conflicts:
	core/jni/Android.mk
Change-Id: I580222f689f7cd677bb5c5f0f8919624640897d3

libs/androidfw/Android.mk
@@ -51,6 +51,8 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_CFLAGS += -DSTATIC_ANDROIDFW_FOR_TOOLS
 
+LOCAL_CPPFLAGS += -fno-strict-aliasing
+
 LOCAL_C_INCLUDES := \
 	external/zlib
 
@@ -78,6 +80,8 @@ LOCAL_STATIC_LIBRARIES := libziparchive
 LOCAL_C_INCLUDES := \
     external/zlib \
     system/core/include
+
+LOCAL_CPPFLAGS += -fno-strict-aliasing
 
 LOCAL_MODULE:= libandroidfw
 