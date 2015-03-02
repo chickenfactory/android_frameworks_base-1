CyanogenMod/android_frameworks_base

Add left QS quick pulldown & fix multiuser (1/2)
Change-Id: Id0251dbaa16359ef01c1f13ba2443946ceb53b14

core/java/android/provider/Settings.java
@@ -1249,9 +1249,11 @@ public final class Settings {
             /* CM12 CHANGES */
             MOVED_TO_SECURE.add(Secure.STATS_COLLECTION);
             MOVED_TO_SECURE.add(Secure.VOLUME_LINK_NOTIFICATION);
+            MOVED_TO_SECURE.add(Secure.QS_QUICK_PULLDOWN);
             MOVED_TO_SECURE.add(Secure.QS_TILES);
             MOVED_TO_SECURE.add(Secure.QS_USE_MAIN_TILES);
             MOVED_TO_SECURE.add(Secure.QS_SHOW_BRIGHTNESS_SLIDER);
+            MOVED_TO_SECURE.add(Secure.DOUBLE_TAP_SLEEP_GESTURE);
             for (String s : Secure.NAVIGATION_RING_TARGETS) {
                 MOVED_TO_SECURE.add(s);
             }
@@ -2166,9 +2168,19 @@ public final class Settings {
 
         /**
          * Whether to allow one finger quick settings expansion on the right side of the statusbar.
+         * @deprecated Use {@link android.provider.Settings.Secure#QS_QUICK_PULLDOWN} instead
          * @hide
          */
+        @Deprecated
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
+        /**
+         * Quick Settings Quick Pulldown
+         * @deprecated Use {@link android.provider.Settings.Secure#QS_QUICK_PULLDOWN} instead
+         * @hide
+         */
+        @Deprecated
+        public static final String QS_QUICK_PULLDOWN = Secure.QS_QUICK_PULLDOWN;
 
         /**
          * Whether to show the brightness slider in quick settings panel.
@@ -3373,10 +3385,12 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /**
-         *  Enable statusbar double tap gesture on to put device to sleep
+         * Enable statusbar double tap gesture on to put device to sleep
+         * @deprecated Use {@link android.provider.Settings.Secure#DOUBLE_TAP_SLEEP_GESTURE} instead
          * @hide
          */
-        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+        @Deprecated
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = Secure.DOUBLE_TAP_SLEEP_GESTURE;
 
         /**
          * Boolean value on whether to show weather in the statusbar
@@ -5830,6 +5844,12 @@ public final class Settings {
         public static final String APP_PERFORMANCE_PROFILES_ENABLED = "app_perf_profiles_enabled";
 
         /**
+         * Quick Settings Quick Pulldown
+         * @hide
+         */
+        public static final String QS_QUICK_PULLDOWN = "qs_quick_pulldown";
+
+        /**
          * Whether to show the brightness slider in quick settings panel.
          * @hide
          */
@@ -5853,6 +5873,12 @@ public final class Settings {
          * @hide
          */
         public static final String QS_LOCATION_ADVANCED = "qs_location_advanced";
+
+        /**
+         * Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
          * Custom navring actions

packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -77,7 +77,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // database gets upgraded properly. At a minimum, please confirm that 'upgradeVersion'
     // is properly propagated through your change.  Not doing so will result in a loss of user
     // settings.
-    private static final int DATABASE_VERSION = 123;
+    private static final int DATABASE_VERSION = 124;
 
     private Context mContext;
     private int mUserHandle;
@@ -1967,6 +1967,29 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             upgradeVersion = 123;
         }
 
+        if (upgradeVersion < 124) {
+            // Migrate from cm-11.0
+            moveSettingsToNewTable(db, TABLE_SYSTEM, TABLE_SECURE,
+                    new String[] { Settings.Secure.QS_QUICK_PULLDOWN }, true);
+
+            // Migrate from cm-12.0 if there is no entry from cm-11.0
+            db.beginTransaction();
+            SQLiteStatement stmt = null;
+            try {
+                stmt = db.compileStatement("INSERT OR IGNORE INTO secure(name,value)"
+                        + " VALUES(?,?);");
+                int quickPulldown = getIntValueFromSystem(db,
+                        Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
+                        R.integer.def_qs_quick_pulldown);
+                loadSetting(stmt, Settings.Secure.QS_QUICK_PULLDOWN, quickPulldown);
+                db.setTransactionSuccessful();
+            } finally {
+                db.endTransaction();
+                if (stmt != null) stmt.close();
+            }
+            upgradeVersion = 124;
+        }
+
         // *** Remember to update DATABASE_VERSION above!
 
         if (upgradeVersion != currentVersion) {
@@ -2612,6 +2635,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             loadBooleanSetting(stmt, Settings.Secure.ADVANCED_MODE,
                     R.bool.def_advanced_mode);
+
+            loadIntegerSetting(stmt, Settings.Secure.QS_QUICK_PULLDOWN,
+                    R.integer.def_qs_quick_pulldown);
 
             loadDefaultThemeSettings(stmt);
             loadProtectedSmsSetting(stmt);

chickenfactory/android_frameworks_base-1

Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

core/java/android/provider/Settings.java
@@ -2207,9 +2207,18 @@ public final class Settings {
 
         /**
          * Whether to allow one finger quick settings expansion on the right side of the statusbar.
+         * @deprecated Use {@link android.provider.Settings.System#QS_QUICK_PULLDOWN} instead
          * @hide
          */
-        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+        @Deprecated
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = System.QS_QUICK_PULLDOWN;
+
+        /**
+         * Quick Settings Quick Pulldown
+         * 0 = off, 1 = right, 2 = left
+         * @hide
+         */
+        public static final String QS_QUICK_PULLDOWN = "qs_quick_pulldown";
 
         /**
          * Whether to show the brightness slider in quick settings panel.
@@ -3486,7 +3495,7 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /**
-         *  Enable statusbar double tap gesture on to put device to sleep
+         * Enable statusbar double tap gesture on to put device to sleep
          * @hide
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";

packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -77,7 +77,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // database gets upgraded properly. At a minimum, please confirm that 'upgradeVersion'
     // is properly propagated through your change.  Not doing so will result in a loss of user
     // settings.
-    private static final int DATABASE_VERSION = 123;
+    private static final int DATABASE_VERSION = 124;
 
     private Context mContext;
     private int mUserHandle;
@@ -1971,6 +1971,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             upgradeVersion = 123;
         }
 
+        if (upgradeVersion < 124) {
+            // Migrate from cm-12.0 if there is no entry from cm-11.0
+            db.beginTransaction();
+            SQLiteStatement stmt = null;
+            try {
+                stmt = db.compileStatement("INSERT OR IGNORE INTO secure(name,value)"
+                        + " VALUES(?,?);");
+                int quickPulldown = getIntValueFromSystem(db,
+                        Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
+                        R.integer.def_qs_quick_pulldown);
+                loadSetting(stmt, Settings.System.QS_QUICK_PULLDOWN, quickPulldown);
+                db.setTransactionSuccessful();
+            } finally {
+                db.endTransaction();
+                if (stmt != null) stmt.close();
+            }
+            upgradeVersion = 124;
+        }
+
         // *** Remember to update DATABASE_VERSION above!
 
         if (upgradeVersion != currentVersion) {
@@ -2483,6 +2502,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             loadIntegerSetting(stmt, Settings.System.ENABLE_PEOPLE_LOOKUP,
                     R.integer.def_people_lookup);
+
+            loadIntegerSetting(stmt, Settings.System.QS_QUICK_PULLDOWN,
+                    R.integer.def_qs_quick_pulldown);
 
         } finally {
             if (stmt != null) stmt.close();